2fda49624aab7f42e371f594533fd7d20b9a9c76
Roopashree CHANDRA MOHAN <roopashree.chandramohan@amadeus.com>
2020-10-13 23:16:44 +0530

    - modified code to handle insertion of last element

--- src/main/java/com/rcm/dataStructure/arrays/MergeSortedArrays.java
+++ src/main/java/com/rcm/dataStructure/arrays/MergeSortedArrays.java
@@ -3,17 +3,9 @@ package com.rcm.dataStructure.arrays;
 public class MergeSortedArrays {
 
     public static void main(String[] args) {
-        int[] arr1 = {1, 4, 8, 13, 22};
+        int[] arr1 = {1, 4, 8, 13, 22, 30, 31};
         int[] arr2 = {2, 7, 11, 12, 30};
         sort(arr1, arr2);
-
-
-
-
-
-
-
-
     }
 
     public static void sort(int arr1[], int arr2[]) {
@@ -24,16 +16,22 @@ public class MergeSortedArrays {
         int v2 = arr2[0];
         int index2 = 0;
         int index3 = 0;
+        boolean a1Updated = false;
+        boolean a2Updated = false;
 
         while(index1 < arr1.length && index2 < arr2.length) {
+            a1Updated = false;
+            a2Updated = false;
             if(arr1[index1] < arr2[index2]) {
                 sortedArray[index3] = arr1[index1];
                 index3++;
                 index1++;
+                a1Updated = true;
             } else if(arr1[index1] > arr2[index2]) {
                 sortedArray[index3] = arr2[index2];
                 index3++;
                 index2++;
+                a2Updated = true;
             } else {
                 sortedArray[index3] = arr1[index1];
                 index3++;
@@ -41,7 +39,14 @@ public class MergeSortedArrays {
                 index3++;
                 index1++;
                 index2++;
+                a1Updated = true;
+                a2Updated = true;
             }
+        }
+        if(!a1Updated) {
+            sortedArray[index3] = arr1[index1];
+        } else if(!a2Updated) {
+            sortedArray[index3] = arr2[index2];
         }
         for(int i : sortedArray) {
             System.out.print(i + " ");
