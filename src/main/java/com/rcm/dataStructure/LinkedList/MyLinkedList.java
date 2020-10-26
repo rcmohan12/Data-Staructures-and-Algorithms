485d2856897f698642c6dfbec3ac44509385755c
rcmohan12 <roopamohan12@gmail.com>
2020-10-26 22:40:22 +0530

    - added append method that adds new values to the end of the LL

--- src/main/java/com/rcm/dataStructure/LinkedList/MyLinkedList.java
+++ src/main/java/com/rcm/dataStructure/LinkedList/MyLinkedList.java
@@ -9,11 +9,29 @@ public class MyLinkedList {
     private long _length;
 
 
+    /**
+     * Constructer initializing the head and the tail values.
+     * When the LL is created for the first time, the head and the tail point to the same object
+     * @param val
+     */
     MyLinkedList(String val) {
         this._head = new LinkedNode();
         this._head.setVal(val);
         this._tail = this._head;
         this._length = 1;
+    }
+
+    /**
+     * Use this method to add to the end of the LL
+     * The head remains constant whereas the tail changes to refer to the last added value to the LL
+     * @param val
+     */
+    public void append(String val) {
+        LinkedNode newNode = new LinkedNode();
+        newNode.setVal(val);
+        this._tail.setNext(newNode);
+        this._tail = newNode;
+        _length++;
     }
 
 
@@ -26,6 +44,9 @@ public class MyLinkedList {
     }
 }
 
+/**
+ * Object that acts a node that  will store the value to the list and a reference to the next node in the LL
+ */
 class LinkedNode {
 
     String val;
@@ -59,7 +80,11 @@ class LinkedNode {
 class TestMyLinkedList {
 
     public static void main(String[] args) {
-        MyLinkedList myLL = new MyLinkedList("Roopa");
+        MyLinkedList myLL = new MyLinkedList("Sie");
+        myLL.append("ist");
+        myLL.append("eine");
+        myLL.append("Schone");
+        myLL.append("Frau");
         System.out.println("My Linked List :"+myLL);
     }
 
