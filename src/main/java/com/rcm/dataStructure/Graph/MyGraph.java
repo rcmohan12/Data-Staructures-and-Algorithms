2f9dad57f0eba0beb62eb18c373a486ac4913837
rcmohan12 <roopamohan12@gmail.com>
2020-11-29 01:16:28 +0530

    - implementation for graph

---
+++ src/main/java/com/rcm/dataStructure/Graph/MyGraph.java
@@ -0,0 +1,118 @@
+package com.rcm.dataStructure.Graph;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Custom class that implements a Graph using an adjacency list
+ * Making use of HashTable available in the Java API
+ */
+public class MyGraph {
+
+    private int _size;
+    private Hashtable<Integer, List<GraphNode>> _adjacentList;
+
+    MyGraph() {
+        this._adjacentList = new Hashtable<>();
+    }
+
+    public boolean addVertex(int val) {
+        GraphNode node = new GraphNode(val);
+        if(!_adjacentList.containsKey(val)) {
+            _adjacentList.put(val, new ArrayList<>());
+            _size++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addEdge(int v1, int v2) {
+        if(_adjacentList.containsKey(v1) && _adjacentList.containsKey(v2)) {
+            List<GraphNode> list = _adjacentList.get(v1);
+            GraphNode edge = new GraphNode(v2);
+            if(list.contains(edge)) {
+                return false;
+            } else {
+                list.add(edge);
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MyGraph{" +
+                "_size=" + _size +
+                ", _adjacentList=" + _adjacentList +
+                '}';
+    }
+}
+
+class GraphNode {
+
+    int value;
+
+    GraphNode() {
+
+    }
+
+    public GraphNode(int val) {
+        this.value = val;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        GraphNode graphNode = (GraphNode) o;
+        return value == graphNode.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "GraphNode{" +
+                "value=" + value +
+                '}';
+    }
+}
+
+class TestMyGraph {
+    public static void main(String[] args) {
+        MyGraph graph = new MyGraph();
+        System.out.println("Add 0: "+graph.addVertex(0));
+        System.out.println("Add 1: "+graph.addVertex(1));
+        System.out.println("Add 2: "+graph.addVertex(2));
+        System.out.println("Add 3: "+graph.addVertex(3));
+        System.out.println("Add 4: "+graph.addVertex(4));
+        System.out.println("Add 5: "+graph.addVertex(5));
+        System.out.println("Add 6: "+graph.addVertex(6));
+        System.out.println("Adding edge 3, 1 : "+graph.addEdge(3,1));
+        System.out.println("Adding edge 3, 4 : "+graph.addEdge(3,4));
+        System.out.println("Adding edge 3, 4 : "+graph.addEdge(3,4));
+        System.out.println("Adding edge 4, 2 : "+graph.addEdge(4,2));
+        System.out.println("Adding edge 4, 5 : "+graph.addEdge(4,5));
+        System.out.println("Adding edge 1, 2 : "+graph.addEdge(1,2));
+        System.out.println("Adding edge 1, 0 : "+graph.addEdge(1,0));
+        System.out.println("Adding edge 0, 2 : "+graph.addEdge(0,2));
+        System.out.println("Adding edge 6, 5 : "+graph.addEdge(6,5));
+
+
+        System.out.println(graph);
+    }
+}
