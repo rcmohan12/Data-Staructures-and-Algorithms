0fd5d6d394a547bd51c3e3103c802798f917bdba
rcmohan12 <roopamohan12@gmail.com>
2020-11-30 20:19:14 +0530

    - Fibonacci number using recursion

---
+++ src/main/java/com/rcm/algorithms/Recursion.java
@@ -0,0 +1,23 @@
+package com.rcm.algorithms;
+
+public class Recursion {
+
+    public static void main(String[] args) {
+        System.out.println("Fibonacci value at 8 :"+getFiboNum(8));
+    }
+
+    /**
+     * This method returns the value of the Fibonacci number at the specified index position
+     * Time complexity : O(2^n)
+     * @param index
+     * @return
+     */
+    public static int getFiboNum(int index) {
+
+        if(index < 2) {
+            return index;
+        }
+        return getFiboNum(index-1)+getFiboNum(index-2);
+
+    }
+}
