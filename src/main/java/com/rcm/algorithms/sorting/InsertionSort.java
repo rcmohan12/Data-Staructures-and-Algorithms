55ef872ee69b9bfee8cee0bb153041dddb6ec2e6
rcmohan12 <roopamohan12@gmail.com>
2020-12-05 15:52:19 +0530

    - refined impl for insertion sort

--- src/main/java/com/rcm/algorithms/sorting/InsertionSort.java
+++ src/main/java/com/rcm/algorithms/sorting/InsertionSort.java
@@ -7,18 +7,32 @@ public class InsertionSort {
         sort(vals);
     }
 
-
+    /**
+     * Time complexity - O(n^2)  ******could be close to O(n) if the list is almost sorted
+     * Space complexity - O(1)
+     * @param vals
+     * @return
+     */
     private static int[] sort(int[] vals) {
         int[] sorted = vals;
-
-        for(int i = 0; i < sorted.length; i++) {
-            if(sorted[i] > sorted[i+1]) {
-                for(int j = i; j >=0; j--) {
-                    if()
+        for(int i = 1; i < sorted.length; i++) { //O(n)
+            if(sorted[i] < sorted[i-1]) {
+                for(int j = i; j >= 0; j--) { //O(n)
+                    if(j == 0) {
+                        break;
+                    } else if(sorted[j] < sorted[j-1]) {
+                        int temp = sorted[j-1];
+                        sorted[j-1] = sorted[j];
+                        sorted[j] = temp;
+                    } else {
+                        break;
+                    }
                 }
             }
         }
-
+        for(int x = 0; x < sorted.length; x++) {
+            System.out.print(sorted[x] + " ");
+        }
         return sorted;
     }
 
