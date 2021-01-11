87d409d49e12ee9bfee5b52a1a047171e395a8ec
rcmohan12 <roopamohan12@gmail.com>
2021-01-12 00:08:02 +0530

    - Queue Implementation

--- src/main/java/com/rcm/dataStructure/queue/MyQueue.java
+++ src/main/java/com/rcm/dataStructure/queue/MyQueue.java
@@ -1,4 +1,105 @@
 package com.rcm.dataStructure.queue;
 
+
 public class MyQueue {
+
+    private QueueNode _first; // Most recent element added to the queue
+    private QueueNode _last; // The very first element added to the queue
+    private long _length;
+
+    MyQueue() {
+        this._first = this._last;
+    }
+
+    public void enqueue(String val) {
+        QueueNode node = new QueueNode();
+        node.setVal(val);
+        if(this._first == null) {
+            this._first = node;
+            this._last = this._first;
+        } else {
+            this._first.setNext(node);
+            this._first = node;
+        }
+        _length++;
+    }
+
+    public String dequeue() {
+        if(this._last != null) {
+            String val = this._last.getVal();
+            QueueNode bottom = this._last;
+            this._last = this._last.getNext();
+            bottom.setNext(null);
+            _length--;
+            return val;
+        } else {
+            return "Queue is empty";
+        }
+    }
+
+    public String peek() {
+        if(this._last != null) {
+            return this._last.getVal();
+        } else {
+            return "Queue is empty";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MyQueue{" +
+                "_top=" + _first +
+                ", _bottom=" + _last +
+                ", _length=" + _length +
+                '}';
+    }
+}
+
+class QueueNode {
+
+    String val;
+    QueueNode next;
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+    public QueueNode getNext() {
+        return next;
+    }
+
+    public void setNext(QueueNode next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "QueueNode{" +
+                "val='" + val + '\'' +
+                ", next=" + next +
+                '}';
+    }
+}
+
+class TestMyQueue {
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+        queue.enqueue("first");
+        queue.enqueue("second");
+        queue.enqueue("third");
+        System.out.println("Peek---->"+queue.peek());
+        System.out.println(queue.dequeue());
+        System.out.println("Peek---->"+queue.peek());
+        System.out.println(queue.dequeue());
+        System.out.println("Peek---->"+queue.peek());
+        System.out.println(queue.dequeue());
+        System.out.println("Peek---->"+queue.peek());
+        System.out.println(queue.dequeue());
+
+        System.out.println(queue);
+    }
 }
--- src/main/java/com/rcm/dataStructure/queue/QueueUsingStack.java
+++ src/main/java/com/rcm/dataStructure/queue/QueueUsingStack.java
@@ -1,4 +1,73 @@
 package com.rcm.dataStructure.queue;
+import com.rcm.dataStructure.Stack.MyStack;
 
 public class QueueUsingStack {
+    private MyStack _pushStack;
+    private MyStack _popStack;
+
+   QueueUsingStack() {
+       _pushStack = new MyStack();
+       _popStack = new MyStack();
+   }
+
+   public void enqueue(String val) {
+       _pushStack.push(val);
+   }
+
+   public String dequeue() {
+       if(_popStack.isEmpty()) {
+           if(_pushStack.isEmpty()) {
+               return "The queue is empty";
+           } else {
+               while (!_pushStack.isEmpty()) {
+                   _popStack.push(_pushStack.pop());
+               }
+               return _popStack.pop();
+           }
+       } else {
+           return _popStack.pop();
+       }
+   }
+
+   public String peek() {
+       if(_popStack.isEmpty()) {
+           if(_pushStack.isEmpty()) {
+               return "The queue is empty";
+           } else {
+               while (!_pushStack.isEmpty()) {
+                   _popStack.push(_pushStack.pop());
+               }
+               return _popStack.peek();
+           }
+       } else {
+           return _popStack.peek();
+       }
+   }
+
+    @Override
+    public String toString() {
+        return "QueueUsingStack{" +
+                "stack=" + _pushStack +
+                '}';
+    }
+}
+
+class TestThis {
+    public static void main(String[] args) {
+        QueueUsingStack qs = new QueueUsingStack();
+        qs.enqueue("Sie");
+        qs.enqueue("ist");
+        qs.enqueue("eine");
+        System.out.println("Peek ---->"+qs.peek());
+        System.out.println(qs.dequeue());
+        System.out.println("Peek ---->"+qs.peek());
+        System.out.println(qs.dequeue());
+        System.out.println("Peek ---->"+qs.peek());
+        System.out.println(qs.dequeue());
+        qs.enqueue("schone");
+        qs.enqueue("frau");
+        System.out.println(qs);
+        System.out.println(qs.dequeue());
+        System.out.println(qs.dequeue());
+    }
 }
